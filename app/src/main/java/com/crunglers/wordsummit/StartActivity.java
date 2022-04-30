9d8b673f8d7c24a9655b245315a55cfec7a3106e
jetson foster <jetsonfoster@d142-058-162-131.wireless.sfu.ca>
2022-04-30 13:42:36 -0700

    George

--- app/src/main/java/com/crunglers/wordsummit/StartActivity.java
+++ app/src/main/java/com/crunglers/wordsummit/StartActivity.java
@@ -1,5 +1,7 @@
 package com.crunglers.wordsummit;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -11,12 +13,17 @@ import android.view.WindowManager;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class StartActivity extends AppCompatActivity {
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        player = MediaPlayer.create(this,R.raw.wm_music);
+        player.setLooping(true);
+        player.start();
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         super.onCreate(savedInstanceState);
