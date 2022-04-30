25f4f1139b8956ffc12ddde46232a7043bf61c79
Efe Erhan <efee@sfu.ca>
2022-04-30 03:56:24 -0700

    fucked around with font in dialog box

--- app/src/main/java/com/crunglers/wordsummit/SynonymDialogFragment.java
+++ app/src/main/java/com/crunglers/wordsummit/SynonymDialogFragment.java
@@ -3,7 +3,9 @@ package com.crunglers.wordsummit;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,11 +15,18 @@ public class SynonymDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return new AlertDialog.Builder(requireContext())
+        AlertDialog AD = new AlertDialog.Builder(requireContext())
                 .setMessage("balls")
                 .setPositiveButton("Next", (dialog, which) -> {
                     Intent i = new Intent(getContext(), GameActivity.class);
                     requireContext().startActivity(i);} )
+                .setNegativeButton("Go back", (dialog, which) -> {
+                    dismiss();
+                })
                 .create();
+
+        /*TextView textView = (TextView)AD.findViewById(android.R.id.message);
+        textView.setTypeface(Typeface.createFromAsset(getContext().getAssets(),"font/visbyround.otf"));*/
+        return AD;
     }
 }
